38f851aaf75851f972bed80a1fec7138c7298ebe
Erick Hernandez <ehern131@fiu.edu>
2017-01-10 18:49:48 -0500

    Angular directives & filters

--- public/assets/JavaScript/app.js
+++ public/assets/JavaScript/app.js
@@ -7,16 +7,38 @@
 
   var gems = [
     {
-      name:"Pentagonal Gem",
-      price: 5.95,
-      description: "bruh",
-      canPurchase: true,
+      name:"Emerald Gem",
+      price: 1.50,
+      description: "Colored green by trace amounts of chromium and sometimes vanadium.",
+      // canPurchase: true
+      images: [
+        {
+          full: "http://www.gifmania.us/Objects/Jewelry/Gemstones/Emeralds/Emerald-88746.gif",
+          thumb: "http://www.gifmania.us/Objects/Jewelry/Gemstones/Emeralds/Emerald-88746.gif"
+        },
+        {
+          full: "http://www.gifmania.us/Objects/Jewelry/Gemstones/Emeralds/Emerald-88746.gif",
+          thumb: "http://www.gifmania.us/Objects/Jewelry/Gemstones/Emeralds/Emerald-88746.gif"
+        }
+      ]
     },
     {
       name: 'Dodecahedron',
       price: 2.95,
-      description: '......',
-      canPurchase: false,
+      description: 'A polyhedron with twelve flat faces.',
+      // canPurchase: false
+      images: [
+        {
+          full: 'http://math.ucr.edu/home/baez/dodecahedron/Dodecahedron.gif',
+          thumb: 'http://math.ucr.edu/home/baez/dodecahedron/Dodecahedron.gif'
+        },
+        {
+          full: 'https://thesithlibrary.files.wordpress.com/2008/03/rotating_dodecahedron.gif',
+          thumb: 'https://thesithlibrary.files.wordpress.com/2008/03/rotating_dodecahedron.gif',
+        }
+      ]
     }
   ];
+
+
 })();
